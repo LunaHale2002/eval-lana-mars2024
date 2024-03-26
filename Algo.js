7ae51c31b4a476c7258cc0bbe900ffb35aa80cee
LunaHale2002 <lbabic@edenschool.fr>
2024-03-26 16:50:49 +0100

    Algo

---
+++ Algo.js
@@ -0,0 +1,193 @@
+// Exercice 1.
+
+// function isString(myString) {
+//     if (typeof (myString) === "String") {
+//         return true;
+//     } else {
+//         return false;
+//     }
+// }
+// console.log(isString("Marc"));
+// console.log(isString(2));
+
+// Exercice 2
+
+// function greetings(isStudent) {
+//     if (isStudent == true) {
+//         console.log("Bienvenue cher élèves.");
+//     } else {
+//         console.log("Bienvenue cher parents.");
+//     }
+// }
+// console.log(greetings(true));
+// console.log(greetings(false));
+
+// Exercice 3
+
+// function checkWord(word) {
+//     word = "";
+//     if (word !== 3) {
+//         console.log("Ce mot est trop court");
+//     } else {
+//         console.log("Le mot" + {word} + "est tout à fait correct");
+//     }
+// }
+// console.log(checkWord("qw"));
+// console.log(checkWord("pizza"));
+
+// Exercice 4
+
+// function makeSum(number1, number2) {
+//     return [number1 + number2;]
+// }
+//console.log(makeSum(2, 3));
+
+// Exercice 5
+
+// function makeSumFromArray(numbers) {
+//     for (let i = 0; i < numbers.length; i++) {
+//         return numbers + numbers;
+//     }
+// }
+// console.log(makeSumFromArray([1, 2, 3, 5]));
+
+// Exercice 6
+
+// function findOddNumbers(numbers) {
+//     let arr = [];
+//     for (let i = 0; i < numbers.length; i ++) {
+//         arr += numbers[i];
+//     }
+// }
+// console.log(findOddNumbers([1, 2, 5, 7, 8, 11, 13, 16, 19, 20, 21]));
+
+// Exercice 7
+
+// function filterArray(myArray) {
+//     let arr = [];
+//     for (let i = 0; i < myArray.length; i++) {
+//     }
+// }
+// console.log(filterArray([1, 2, true, "Tom", "Max", false, 4, 5, 6, "John"]));
+
+// Exercice 8
+
+// function concatString(arrayOfStrings) {
+// }
+// console.log(concatString(["Un", " ", "code", " ", "de", " ", "qualité"]));
+
+// Exercice 9
+
+// function findElementByWord(arrayOfStrings, term) {
+//     for (let i = 0; i < findElementByWord.length; i++) {
+//         if (term) {
+//             return true;
+//         } else {
+//             return false;
+//         }
+//     }
+// }
+// console.log(findElementByWord(["max", "peter", "john"], "peter"));
+// console.log(findElementByWord(["max", "peter", "john"], "tom"));
+
+// Exercice 10
+
+// function lowerCaseWords(arrayOfStrings) {
+// }
+// console.log(lowerCaseWords(["uN", " jOLI", " CheVAl ", "BlANc"]));
+
+// Exercice 11
+
+function mergeArray(array1, array2) {
+    for (let i = 0; i < array1.length; i++) {
+    }
+}
+console.log(mergeArray([1, 3, 2], [4, 1, 3, 2]));
+
+// Exercice 12
+
+// function makeFriends(friends) {
+// }
+
+// const myFriends = [
+//     {
+//         firstName: "Max",
+//         lastName: "Verstappen",
+//         age: 27
+//     },
+//     {
+//         firstName: "Lewis",
+//         lastName: "Hamilton",
+//         age: 37
+//     },
+//     {
+//         firstName: "Daniel",
+//         lastName: "Ricciardo",
+//         age: 36
+//     }
+// ]
+// console.log(makeFriends(myFriends));
+
+// Exercice 13
+
+// function findTeamPrincipal(people) {
+// }
+
+// let somePeople = [
+//     {
+//         firstName: 'Toto',
+//         lastName: 'Wolff',
+//         isTeamPrincipal: true
+//     },
+//     {
+//         firstName: 'Lando',
+//         lastName: 'Noris',
+//         isTeamPrincipal: false
+//     },
+//     {
+//         firstName: 'Oscar',
+//         lastName: 'Piastri',
+//         isTeamPrincipal: false
+//     },
+//     {
+//         firstName: 'Christian',
+//         lastName: 'Horner',
+//         isTeamPrincipal: true
+//     }
+// ]
+// console.log(findTeamPrincipal(somePeople));
+
+// Exercice 14
+
+// function findCar(cars, query) {
+// }
+
+// let someCars = [
+//     {
+//         driver: "Esteban Ocon",
+//         brand: "Alpine",
+//         isFast: false
+//     },
+//     {
+//         driver: "Charles Leclerc",
+//         brand: "Ferrari",
+//         isFast: true
+//     },
+//     {
+//         driver: "Sergio Perrez",
+//         brand: "Red Bull",
+//         isFast: true
+//     },
+//     {
+//         driver: "Pierre Gasli",
+//         brand: "Alpine",
+//         isFast: false
+//     }
+// ]
+// console.log(findCar(someCars, "Alpine")); 
+
+// Exercice 15 (BONUS)
+
+// function repeatString(str, num) {
+// }
+// console.log(repeatString("qsdf", 3));
